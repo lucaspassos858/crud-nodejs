1dc3d03451efeb3e99e8db6fbd6bcc4aa0647ef3
lucaspassos858 <lucaspassos858@gmail.com>
2020-11-12 20:56:58 -0300

    crud js

--- __tests__/integration/sessison.test.js
+++ __tests__/integration/sessison.test.js
@@ -19,9 +19,8 @@ describe('create user', () => {
 
 describe('list user', () => {
     it('should list the created user', async (done) => {
-        const user = await User.create({ name: 'Lucas', email: 'lucas@email.com', password_hash: '123456' });
-        const listedUser = await controller.listUser('lucas@email.com');
-        console.log(listedUser);
+        const user = await User.create({ name: 'Lucas', email: 'lucaslist@email.com', password_hash: '123456' });
+        const listedUser = await controller.listUser('lucaslist@email.com');
 
         expect(listedUser).toEqual(
             expect.objectContaining({
@@ -35,9 +34,9 @@ describe('list user', () => {
 
 describe('delete user', () => {
     it('should delete a created user', async (done) => {
-        const user = await User.create({ name: 'Lucas', email: 'lucas@email.com', password_hash: '123456' });
+        const user = await User.create({ name: 'Lucas', email: 'lucasdelete@email.com', password_hash: '123456' });
 
-        const userToDelete = await controller.deleteUser('lucas@email.com');
+        const userToDelete = await controller.deleteUser('lucasdelete@email.com');
 
         expect(userToDelete).toBeTruthy();
 
@@ -46,3 +45,13 @@ describe('delete user', () => {
 });
 
 
+describe('update user', () => {
+    it('should update a created user', async (done) => {
+        const user = await User.create({ name: 'Lucas', email: 'lucasupdate@email.com', password_hash: '123456' });
+
+        const userToUpdate = await controller.updateUser('lucasupdate@email.com', 'Lucas Update');
+
+        expect(userToUpdate.name).toBe('Lucas Update');
+        done();
+    });
+});
--- src/app/controller/controller.js
+++ src/app/controller/controller.js
@@ -11,5 +11,15 @@ module.exports = {
         const result = await User.destroy({ where: { email: email } });
         console.log(result);
         return result;
+    },
+
+    async updateUser(email,newName) {
+        await User.update({ name: newName }, {
+            where: {
+              email: email
+            }
+          });
+          const result = await User.findOne({ where: { name: newName } });
+        return result;
     }
 }
